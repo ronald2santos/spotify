18cf71c11d7ac619f80bc64059c8c6f830bad9b6
ronald2santos <ronald.j.dos.santos@gmail.com>
2020-01-03 13:48:13 -0300

    Forgot to change branches, added carousel to overview page, switched back to local environment

--- src/app/components/overview/overview.component.ts
+++ src/app/components/overview/overview.component.ts
@@ -3,7 +3,7 @@ import { TrackService } from '../../services/track.service';
 import { ArtistService } from '../../services/artist.service';
 import { SpotifyService } from '../../services/spotify.service';
 import { Artist } from 'src/app/typing/artist';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { fadeInAnimation } from '../../router.animation';
 
 
@@ -16,9 +16,35 @@ import { fadeInAnimation } from '../../router.animation';
 export class OverviewComponent implements OnInit {
 
   // tslint:disable-next-line: max-line-length
-  constructor(private trackService: TrackService, private artistService: ArtistService, private spotify: SpotifyService, private route: ActivatedRoute) { }
+  constructor(private router: Router, private trackService: TrackService, private artistService: ArtistService, private spotify: SpotifyService, private route: ActivatedRoute) {
+    this.responsiveOptions = [
+      {
+        breakpoint: '1668px',
+        numVisible: 7,
+        numScroll: 7
+      },
+      {
+        breakpoint: '1400px',
+        numVisible: 6,
+        numScroll: 6
+      },
+      {
+        breakpoint: '950px',
+        numVisible: 4,
+        numScroll: 4
+      },
+      {
+        breakpoint: '560px',
+        numVisible: 3,
+        numScroll: 3
+      }
+    ];
+  }
+  responsiveOptions;
   selectedArtist;
   selectedTrack;
+  topTracks;
+  relatedArtists;
   following: boolean;
   tempo: number;
   duration: number;
@@ -29,6 +55,40 @@ export class OverviewComponent implements OnInit {
   ngOnInit() {
     this.artistService.artistObservable.subscribe((artist) => this.selectedArtist = artist);
     this.trackService.trackObservable.subscribe((track) => this.selectedTrack = track);
+
+    if (this.selectedArtist) {
+      this.getArtistData(this.selectedArtist.id);
+    }
+  }
+
+  onArtistSelect(artist) {
+    this.selectedArtist = artist;
+    this.trackService.setSelectedTrack(null);
+    this.artistService.setSelectedArtist(this.selectedArtist);
+    this.getArtistData(artist.id);
+    // this.router.navigate(['/overview']);
+  }
+
+  onTrackSelect(track) {
+    this.selectedTrack = track;
+    this.artistService.setSelectedArtist(null);
+    this.trackService.setSelectedTrack(this.selectedTrack);
+    // this.router.navigate(['/overview']);
+  }
+
+  getArtistData(artistID) {
+    this.spotify.getArtistsTopTracks(artistID).subscribe(
+      (topTracks) => {
+        console.log(topTracks);
+        this.topTracks = topTracks.tracks;
+      }
+    );
+    this.spotify.getRelatedArtists(artistID).subscribe(
+      (relatedArtists) => {
+        console.log(relatedArtists)
+        this.relatedArtists = relatedArtists.artists;
+      }
+    )
   }
 
   // checkFollowing(id: string) {
